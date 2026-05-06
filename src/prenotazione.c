ad0cf6fd0adde6a66798e7fc4273187cec32bd40
carmine <carmine.picariello024@gmail.com>
2026-05-07 00:02:27 +0200

    validazione data completa con calendario

--- src/prenotazione.c
+++ src/prenotazione.c
@@ -1,66 +1,128 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "../include/prenotazione.h"
+
+// converte stringa in minuscolo
+void toLowerCase(char str[]) {
+    for (int i = 0; str[i]; i++) {
+        str[i] = tolower(str[i]);
+    }
+}
+
+// controlla anno bisestile
+int isBisestile(int anno) {
+    return (anno % 4 == 0 && anno % 100 != 0) || (anno % 400 == 0);
+}
+
+// controlla validità data
+int dataValida(int giorno, int mese, int anno) {
+    if (anno < 1900 || anno > 2100) return 0;
+    if (mese < 1 || mese > 12) return 0;
+    if (giorno < 1) return 0;
+
+    int giorniMese[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    if (mese == 2 && isBisestile(anno)) {
+        giorniMese[1] = 29;
+    }
+
+    if (giorno > giorniMese[mese - 1]) return 0;
+
+    return 1;
+}
 
 void inserisciPrenotazione() {
     Prenotazione p;
-    int risultato_1; 
-    int risultato_2;
-do {
-    printf("Inserisci matricola (max 6 cifre): ");
-    risultato_1 = scanf("%d", &p.matricola);
+    int risultato;
+    char c;
+    int valida;
 
-    if (risultato_1 != 1) {
-        printf("Errore! Inserisci un numero.\n");
-        while (getchar() != '\n');
-        continue;
-    }
+    // 🔹 MATRICOLA
+    do {
+        printf("Inserisci matricola (max 6 cifre): ");
+        risultato = scanf("%d%c", &p.matricola, &c);
 
-    if (p.matricola <= 0 || p.matricola > 999999) {
-        printf("Matricola non valida!\n");
-    }
+        if (risultato != 2 || c != '\n') {
+            printf("Errore! Inserisci SOLO un numero.\n");
+            while (getchar() != '\n');
+            continue;
+        }
 
-} while (risultato_1 != 1 || p.matricola <= 0 || p.matricola > 999999);
-    
+        if (p.matricola <= 0 || p.matricola > 999999) {
+            printf("Matricola non valida!\n");
+        }
+
+    } while (risultato != 2 || c != '\n' || p.matricola <= 0 || p.matricola > 999999);
+
+    // 🔹 POSTO
+    do {
+        printf("Inserisci posto (1-100): ");
+        risultato = scanf("%d%c", &p.posto, &c);
+
+        if (risultato != 2 || c != '\n') {
+            printf("Errore! Inserisci SOLO un numero.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        if (p.posto < 1 || p.posto > 100) {
+            printf("Posto non valido!\n");
+        }
+
+    } while (risultato != 2 || c != '\n' || p.posto < 1 || p.posto > 100);
+
+    // 🔹 DATA COMPLETA
+    int giorno, mese, anno;
 
     do {
-    printf("Inserisci posto (1-100): ");
-    risultato_2 = scanf("%d", &p.posto);
+        valida = 1;
 
-    if (risultato_2 != 1) {
-        printf("Errore! Inserisci un numero.\n");
-        while (getchar() != '\n');
-        continue;
-    }
+        printf("Inserisci data (gg/mm/aaaa): ");
+        scanf("%s", p.data);
 
-    if (p.posto < 1 || p.posto > 100) {
-        printf("Posto non valido!\n");
-    }
+        if (strlen(p.data) != 10 || p.data[2] != '/' || p.data[5] != '/') {
+            valida = 0;
+        }
 
-} while (risultato_2 != 1 || p.posto < 1 || p.posto > 100);
+        // controllo numeri
+        for (int i = 0; i < 10; i++) {
+            if (i == 2 || i == 5) continue;
 
+            if (!isdigit(p.data[i])) {
+                valida = 0;
+            }
+        }
+
+        if (valida) {
+            sscanf(p.data, "%d/%d/%d", &giorno, &mese, &anno);
+
+            if (!dataValida(giorno, mese, anno)) {
+                valida = 0;
+            }
+        }
+
+        if (!valida) {
+            printf("Data non valida!\n");
+        }
+
+    } while (!valida);
+
+    // 🔹 FASCIA ORARIA
     do {
-    printf("Inserisci data (gg/mm/aaaa): ");
-    scanf("%s", p.data);
+        printf("Inserisci fascia (mattina/pomeriggio): ");
+        scanf("%s", p.fascia_oraria);
 
-    if (strlen(p.data) != 10 || p.data[2] != '/' || p.data[5] != '/') {
-        printf("Formato data non valido!\n");
-    }
+        toLowerCase(p.fascia_oraria);
 
-} while (strlen(p.data) != 10 || p.data[2] != '/' || p.data[5] != '/');
+        if (strcmp(p.fascia_oraria, "mattina") != 0 &&
+            strcmp(p.fascia_oraria, "pomeriggio") != 0) {
 
-    do {
-    printf("Inserisci fascia oraria (mattina/pomeriggio): ");
-    scanf("%s", p.fascia_oraria);
+            printf("Fascia non valida!\n");
+        }
 
-    if (strcmp(p.fascia_oraria, "mattina") != 0 &&
-        strcmp(p.fascia_oraria, "pomeriggio") != 0) {
+    } while (strcmp(p.fascia_oraria, "mattina") != 0 &&
+             strcmp(p.fascia_oraria, "pomeriggio") != 0);
 
-        printf("Fascia non valida!\n");
-    }
-
-} while (strcmp(p.fascia_oraria, "mattina") != 0 &&
-         strcmp(p.fascia_oraria, "pomeriggio") != 0);
-
-    printf("Prenotazione inserita!\n");
+    printf("Prenotazione inserita correttamente!\n");
 }
